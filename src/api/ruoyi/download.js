5d9a8a22da7afc54d9343a8d64175295c8b86464
baihuaiyu <342216980@qq.com>
2025-03-31 10:54:27 +0800

    添加RuoYi后端接口

---
+++ src/api/ruoyi/download.js
@@ -0,0 +1,66 @@
+import { ElLoading, ElMessage } from 'element-plus'
+import { saveAs } from 'file-saver'
+import service from '../index'
+
+let downloadLoadingInstance
+
+function tansParams(params) {
+  let result = ''
+  for (const propName of Object.keys(params)) {
+    const value = params[propName]
+    const part = `${encodeURIComponent(propName)}=`
+    if (value !== null && value !== '' && typeof (value) !== 'undefined') {
+      if (typeof value === 'object') {
+        for (const key of Object.keys(value)) {
+          if (value[key] !== null && value[key] !== '' && typeof (value[key]) !== 'undefined') {
+            const params = `${propName}[${key}]`
+            const subPart = `${encodeURIComponent(params)}=`
+            result += `${subPart + encodeURIComponent(value[key])}&`
+          }
+        }
+      }
+      else {
+        result += `${part + encodeURIComponent(value)}&`
+      }
+    }
+  }
+  return result
+}
+
+function blobValidate(data) {
+  return data.type !== 'application/json'
+}
+
+const errorCode = {
+  401: '认证失败，无法访问系统资源',
+  403: '当前操作没有权限',
+  404: '访问资源不存在',
+  default: '系统未知错误，请反馈给管理员',
+}
+
+export function download(url, params, filename, config) {
+  downloadLoadingInstance = ElLoading.service({ text: '正在下载数据，请稍候', background: 'rgba(0, 0, 0, 0.7)' })
+  return service.post(url, params, {
+    transformRequest: [(params) => { return tansParams(params) }],
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    responseType: 'blob',
+    ...config,
+  }).then(async (data) => {
+    const isBlob = blobValidate(data)
+    if (isBlob) {
+      const blob = new Blob([data])
+      saveAs(blob, filename)
+    }
+    else {
+      const resText = await data.text()
+      const rspObj = JSON.parse(resText)
+      const errMsg = errorCode[rspObj.code] || rspObj.msg || errorCode.default
+      ElMessage.error(errMsg)
+    }
+    downloadLoadingInstance.close()
+  }).catch((r) => {
+    console.error(r)
+    ElMessage.error('下载文件出现错误，请联系管理员！')
+    downloadLoadingInstance.close()
+  })
+}
