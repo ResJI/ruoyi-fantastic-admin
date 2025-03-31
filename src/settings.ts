5fc5cf1af1ec51616215efdf1f2dce44b559e79b
baihuaiyu <342216980@qq.com>
2025-03-31 15:26:20 +0800

    修改默认配置及README

--- src/settings.ts
+++ src/settings.ts
@@ -4,7 +4,27 @@ import { merge } from '@/utils/object'
 import { cloneDeep } from 'es-toolkit'
 
 const globalSettings: Settings.all = {
-  // 请在此处编写或粘贴配置代码
+  app: {
+    enablePermission: true,
+    enableDynamicTitle: true,
+  },
+  home: {
+    enable: false,
+  },
+  menu: {
+    enableSubMenuCollapseButton: true,
+    enableHotkeys: true,
+  },
+  tabbar: {
+    enable: true,
+    enableIcon: true,
+    enableHotkeys: true,
+  },
+  toolbar: {
+    fullscreen: true,
+    pageReload: true,
+    colorScheme: true,
+  },
 }
 
 export default merge(globalSettings, cloneDeep(settingsDefault)) as RecursiveRequired<Settings.all>
